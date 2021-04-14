bb37d1fe53d2ccbb5436bb149df11990b53c79ba
erguerra <erguerra.dev@gmail.com>
2021-04-14 02:59:04 -0300

    first version with all algorithms working

--- data/DAO.py
+++ data/DAO.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 
 class DAO:
-    def __init__(self, inputFile, delimiter):
-        self.inputFile = inputFile
+    def __init__(self, input_file, delimiter, output_file):
+        self.inputFile = input_file
         self.delimiter = delimiter
+        self.output_file = output_file
         self.dataset = self.read_data()
 
     def read_data(self):
         return pd.read_csv(self.inputFile, delimiter=self.delimiter)
+
+    def persist_data(self, series):
+        series.to_csv(self.output_file, sep=self.delimiter)
--- main.py
+++ main.py
@@ -1,57 +1,55 @@
+import pandas as pd
+
 from data.BootstrapGenerator import BootstrapGenerator
 from data.DAO import DAO
 from dt.DecisionTree import DecisionTree
 from random_forest.RandomForest import RandomForest
+from validation.KFold import KFold
 
 
 def main():
-    dao_numerical = DAO('data/wine-recognition.tsv', '\t')
-    dao_categorical = DAO('data/house_votes_84.tsv', '\t')
+    dao_benchmark = DAO('data/benchmark.csv', ';', 'results/benchmark_results.tsv')
+    dao_numerical = DAO('data/wine-recognition.tsv', '\t', 'results/numerical_results')
+    dao_categorical = DAO('data/house_votes_84.tsv', '\t', 'results/categorical_results')
 
-    rf = RandomForest(d=dao_categorical.dataset, n=1, target_column_name='target')
-    rf.create_forest()
-    instance = dao_categorical.dataset.loc[86]
-    print(rf.classify_instance(instance))
+    # Code to demonstrate the correct implementation of the decision tree
+    '''
+    dt = DecisionTree(dao_benchmark.dataset, 'Joga')
+    dt.train_tree()
+    dt.print_tree(dt.root_node)
+    instance = dao_benchmark.dataset.iloc[1]
+    instance.drop(labels='Joga', inplace=True)
+    print(instance)
+    print(dt.classify(instance=instance, root_node_tree=dt.root_node))
+    '''
 
-    # rf = RandomForest(d=dao_numerical.dataset, n=30, target_column_name='target')
-    # rf.create_forest()
-    # instance = dao_numerical.dataset.loc[85]
-    # print(rf.classify_instance(instance))
+    forest_sizes = [1, 5, 10, 15, 30, 50]
+
+    results_cathegorical = {}
+    for size in forest_sizes:
+        kfold = KFold(dao_categorical.dataset, 10, target_column_name='target', forest_size=size)
+        kfold.create_folds()
+        results_cathegorical[size] = kfold.cross_validation()
+
+    results_numerical = {}
+    for size in forest_sizes:
+        kfold = KFold(dao_numerical.dataset, 10, target_column_name='target', forest_size=size)
+        kfold.create_folds()
+        results_numerical[size] = kfold.cross_validation()
 
 
-    # targets = df['target'].values
-    # print(targets)
-    # df = df.drop(columns='target')
-    #
-    # classified = []
-    # for i, v in df.iterrows():
-    #     classified.append(dt.classify(v, root_node))
-    # print(classified)
-    #
-    # hits = 0
-    # for i in range(len(classified)):
-    #     if targets[i] == classified[i]:
-    #         hits += 1
-    # print(hits/len(targets))
+    results_categorical_series = pd.Series(data=results_cathegorical)
+    results_numerical_series = pd.Series(data=results_numerical)
 
-    # dao_float = DAO('data/wine-recognition.tsv', 'output.csv')
-    # dao_float.delimiter = '\t'
-    # df_float = dao_float.read_data()
-    #
-    # dt = DecisionTree(df_float, 'target')
-    # dt.split_numerical_data(df_float, '1')
-    # target_column_name = df_float.columns[df_float.columns.size - 1]
-    # instances = [df_float.iloc[0], df_float.iloc[1], df_float.iloc[2], df_float.iloc[3], df_float.iloc[4], df_float.iloc[5], df_float.iloc[6]]
-    # instances_without_target = []
-    # for i in instances:
-    #     instances_without_target.append(i.drop(labels=target_column_name))
-    #
-    # print(f'\n\n\n {dt.l}')
+    dao_categorical.persist_data(results_categorical_series)
+    dao_numerical.persist_data(results_numerical_series)
 
-    # for instance in instances_without_target:
-    #     print(f'Generated class: {dt.classify(instance, root_node)}\n')
+    print('All done!')
 
-    # dt.print_tree(root_node)
+
+
+
+
 
 
 if __name__ == '__main__':
